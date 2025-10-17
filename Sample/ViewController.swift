f3dca7735d62b77fc19fc06635366ad07e961975
neilsardesai <nsardesai@frame.io>
2025-10-17 12:54:07 -0400

    Sample project

--- Sample/ViewController.swift
+++ Sample/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        button.addAction(
+            .init { [weak self] _ in
+                guard let self else { return }
+                let presentedVC = UIViewController()
+                presentedVC.modalPresentationStyle = .popover
+                presentedVC.view.backgroundColor = .clear
+                let popover = presentedVC.popoverPresentationController
+                popover?.sourceItem = button
+                popover?.backgroundColor = .red
+                present(presentedVC, animated: true)
+            },
+            for: .primaryActionTriggered
+        )
     }
-
 
 }
 
